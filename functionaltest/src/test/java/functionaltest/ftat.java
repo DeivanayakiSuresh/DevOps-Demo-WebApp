0dc088253bb01457bebdb5058a38b646b853c33f
DeivanayakiSuresh <70306734+DeivanayakiSuresh@users.noreply.github.com>
2020-09-30 03:41:03 -0700

    updated qa address
    updated qa address

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -17,8 +17,8 @@ public class ftat {
 	//public WebDriver driver;
 	public WebDriverWait wait;
 	//deisur updated ip address for assignment 2
-	public String URL = "http://35.247.33.20:8080/QAWebapp/";
-	public String URL1 = "http://35.247.33.20:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://35.197.37.85:8080/QAWebapp/";
+	public String URL1 = "http://35.197.37.85:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
